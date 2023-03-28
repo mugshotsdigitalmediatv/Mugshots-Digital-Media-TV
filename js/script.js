1e65ed21b7541a0e9f01df5cfeb4c55d4c6436a3
Mariel <mari.mariel121900@gmail.com>
2023-03-28 14:16:34 +0800

    finishi

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
   });
 
     const scriptURL = 'https://script.google.com/macros/s/AKfycbwOb7MEOT-EBA9q16GIwWBz_-yfJeOZgL02Vp8cMZwbK39k3PS7vfy7lne6GHgILyBV/exec'
-  const form = document.forms['submit-to-google-sheet']
+    const form = document.forms['submit-to-google-sheet']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
@@ -45,7 +45,15 @@ $(document).ready(function(){
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
       alert("Message Sent")
-      inputs.clear()
+      document.getElementById('fullname').value = ' ';
+      document.getElementById('emailA').value = ' ';
+      document.getElementById('contactnumber').value = ' ';
+      document.getElementById('messageform').value = ' ';
+      
+      document.getElementById('fullname').placeholder = "Full Name";
+      document.getElementById('emailA').placeholder = "Email Address";
+      document.getElementById('contactnumber').placeholder = "Contact Number"; 
+      document.getElementById('messageform').placeholder = "Message";
       })
 
   
